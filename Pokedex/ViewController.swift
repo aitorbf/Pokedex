14956e65fcd2a4112f248b09bf1ef3a7879cf06c
Aitor Baragaño <aitorbf@gmail.com>
2024-01-26 19:16:13 +0100

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Aitor Baragaño Fernández on 26/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
