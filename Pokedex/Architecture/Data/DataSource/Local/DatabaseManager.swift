6b7c3ece268d7bae04cedf18a01917cd4983c7ec
Aitor Baragaño <aitorbf@gmail.com>
2024-01-27 23:36:23 +0100

    Setup coredata database

---
+++ Pokedex/Architecture/Data/DataSource/Local/DatabaseManager.swift
@@ -0,0 +1,35 @@
+//
+//  DatabaseManager.swift
+//  Pokedex
+//
+//  Created by Aitor Baragaño Fernández on 27/1/24.
+//
+
+import CoreData
+
+protocol DatabaseManager {
+    var container: NSPersistentContainer? { get }
+}
+
+final class DatabaseManagerDefault: DatabaseManager {
+    
+    var container: NSPersistentContainer?
+    
+    init() {
+        initContainer()
+    }
+    
+    private func initContainer() {
+        container = NSPersistentContainer(name: "PokedexDatabase")
+        container?.loadPersistentStores { _, error in
+            if error != nil {
+                self.container = nil
+            }
+        }
+        container?.viewContext.automaticallyMergesChangesFromParent = true
+        container?.viewContext.name = "viewContext"
+        container?.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container?.viewContext.undoManager = nil
+        container?.viewContext.shouldDeleteInaccessibleFaults = true
+    }
+}
